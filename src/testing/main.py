711d18bc9757ee308e96a4f5bc5df1d2336700ed
Jeremy Middleman <jiddleman@gmail.com>
2024-10-07 19:48:06 -0600

    added dataset 2

--- src/testing/main.py
+++ src/testing/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits, load_wine  # Use if importing straight from the website instead of from the downloaded data file
+from sklearn.datasets import load_digits, load_breast_cancer  # Use if importing straight from the website instead of from the downloaded data file
 from sklearn.model_selection import train_test_split
 from utils import incorrectlyClassified, makeCurrentPlot
 from logisticRegression import LogisticRegression
@@ -139,3 +139,77 @@ print(classification_report(y_test_multi, ww_predictions_test_multi))
 
 print("\nConfusion Matrix (Test Data):")
 print(confusion_matrix(y_test_multi, ww_predictions_test_multi))
+
+
+
+
+
+
+# Dataset 2: breast cancer
+print("breast cancer dataset")
+bc = load_breast_cancer()
+X, y = bc.data, bc.target
+
+# Split into train and test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Define the number of features
+num_inputs = X_train.shape[1]
+
+# Generate perceptrons with input values
+lr = LogisticRegression(num_inputs, LEARNING_RATE, NUM_EPOCHS, 0.5, -1, 1)  # Third to last arg is the threshold
+svm = LSVM(LEARNING_RATE, NUM_EPOCHS)
+wh = WidrowHoff(num_inputs, LEARNING_RATE, NUM_EPOCHS, -1, 1)
+
+# Instantiate the Weston Watkins SVM for binary classification
+num_classes = 2  # For binary classification
+regularization = 0.01  # Regularization parameter
+ww_svm = WestonWatkinsSVM(
+    input_size=num_inputs,
+    num_classes=num_classes,
+    learning_rate=LEARNING_RATE,
+    num_epochs=NUM_EPOCHS,
+    min_value=-1,
+    max_value=1,
+    regularization=regularization
+)
+
+# Fit perceptrons
+lr.fit(X_train, y_train)
+svm.fit(X_train, y_train)
+wh.fit(X_train, y_train)
+ww_svm.fit(X_train, y_train)  # Note: Updated to remove num_epochs parameter
+
+# Count the number of misclassified data samples and print
+lr_misclassified_train = incorrectlyClassified(lr, X_train, y_train, "LR")
+lr_misclassified_test = incorrectlyClassified(lr, X_test, y_test, "LR")
+svm_misclassified_train = incorrectlyClassified(svm, X_train, y_train, "SVM")
+svm_misclassified_test = incorrectlyClassified(svm, X_test, y_test, "SVM")
+wh_misclassified_train = incorrectlyClassified(wh, X_train, y_train, "WH")
+wh_misclassified_test = incorrectlyClassified(wh, X_test, y_test, "WH")
+
+# For Weston Watkins SVM, get predictions and calculate misclassified samples
+ww_predictions_train = ww_svm.predict(X_train)
+ww_predictions_test = ww_svm.predict(X_test)
+ww_misclassified_train = np.sum(ww_predictions_train != y_train)
+ww_misclassified_test = np.sum(ww_predictions_test != y_test)
+
+# Number of samples/rows
+num_samples_train = X_train.shape[0]
+num_samples_test = X_test.shape[0]
+
+print("num samples train: ", num_samples_train)
+print("num samples test: ", num_samples_test)
+
+# Results
+print("Logistic Regression train accuracy: ", 1 - (lr_misclassified_train / num_samples_train))
+print("Logistic Regression test accuracy: ", 1 - (lr_misclassified_test / num_samples_test))
+
+print("SVM train accuracy: ", 1 - (svm_misclassified_train / num_samples_train))
+print("SVM test accuracy: ", 1 - (svm_misclassified_test / num_samples_test))
+
+print("WidrowHoff train accuracy: ", 1 - (wh_misclassified_train / num_samples_train))
+print("WidrowHoff test accuracy: ", 1 - (wh_misclassified_test / num_samples_test))
+
+print("Weston Watkins SVM train accuracy: ", 1 - (ww_misclassified_train / num_samples_train))
+print("Weston Watkins SVM test accuracy: ", 1 - (ww_misclassified_test / num_samples_test))
